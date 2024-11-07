90e7e4e239fb9cffa68ee1e79a76ee6dbd9a9354
mahbub.junaidi <mahbub.junaidi@ptap.co.id>
2024-11-07 15:45:30 +0700

    Edit http link

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -4,7 +4,7 @@ const HeaderComponent = () => {
   return (
     <div>
         <nav className='navbar navbar-dark bg-dark'>
-             <a className="navbar-brand" href="http://mahbub,junaidi.net/">Products Management</a>
+             <a className="navbar-brand" href="http://mahbub.junaidi.net/">Products Management</a>
 
         </nav>
     </div>
